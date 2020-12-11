47c5a035698908b940b9322758b567db7705f01f
sergocode <69472815+sergocode@users.noreply.github.com>
2020-12-11 21:50:53 +0300

    Random Color
    Random Color

---
+++ JS/001 Random Color/script.js
@@ -0,0 +1,16 @@
+function sizeValue() {
+    let width = document.getElementById("width").value;
+    let height = document.getElementById("height").value;
+    document.querySelectorAll(".random__result")[0].style.width  = `${width}px`;
+    document.querySelectorAll(".random__result")[0].style.height = `${height}px`;
+}
+
+function randomColor() {
+    const Red   = Math.floor(Math.random() * 256);
+    const Green = Math.floor(Math.random() * 256);
+    const Blue  = Math.floor(Math.random() * 256);
+    document.querySelectorAll(".random__result")[0].style.background = `rgb(${Red}, ${Green}, ${Blue})`;
+}
+
+
+    
