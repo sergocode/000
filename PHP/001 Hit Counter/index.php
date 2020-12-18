e82a3948d09b130e8bbb0467b4d562e57d6a2540
sergocode <69472815+sergocode@users.noreply.github.com>
2020-12-18 12:57:22 +0300

    002 coming soon

---
+++ PHP/001 Hit Counter/index.php
@@ -0,0 +1,20 @@
+<?php
+$PDO = new PDO('mysql:host=localhost;dbname=hit_counter', "root", "root");
+$update_counts = $PDO->query("UPDATE `counter` SET `counts` = `counts` + 1 WHERE `counter`.`id` = 1;");
+$query = $PDO->query("SELECT `counts` FROM `counter` WHERE `counter`.`id` = 1;")->fetchAll(PDO::FETCH_ASSOC);
+foreach ($query as $counts);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">
+    <title>Hit Counter</title>
+</head>
+<body>
+    <div class="hits">Страница была загружена <? echo $counts['counts']; ?> раз. Текущее время <? echo date("H:i") ?>.</div>
+</body>
+</html>
+
+<!--UPDATE counts SET counts = counts + 1-->
